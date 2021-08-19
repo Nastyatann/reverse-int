235f9ae2386d96afa83bad541c907d4465f9fb71
nastyatann <anastasia.kuz.84@gmail.com>
2021-08-19 10:20:32 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);  
+  let str = n.toString();
+  let strReverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+      strReverse += str[i];
+    }
+  return strReverse;
 }
